528f1e476835bb74c0964ade002a3943bb9c7a34
Garrett Dancik <garrett.dancikg@gmail.com>
2023-06-02 11:44:43 -0400

    Need to use 'by' in driver.find_elements

--- mycourses.py
+++ mycourses.py
@@ -1,4 +1,4 @@
-#!/Users/dancikg/opt/anaconda3/bin/python
+#!/Users/dancikg/anaconda3/bin/python
 
 # Get my course schedule
 
@@ -20,28 +20,25 @@ driver.get("https://ssb-prod.ec.easternct.edu/PROD/bwskfcls.p_termsel")
 
 
 # click Advanced search
-elem= driver.find_elements_by_tag_name('input')
+elem= driver.find_elements(by = 'tag name', value = 'input')
 elem[1].click()
 
-both = driver.find_element_by_id('oc_id')
+both = driver.find_element(by = 'id', value = 'oc_id')
 both.click()
-
 
 
 # select 'Computer Science' (direct selection does not work)
 if len(sys.argv) > 1 and sys.argv[1].lower() == '--csc':  
-    select = Select(driver.find_element_by_id('subj_id'))
-    subj = driver.find_element_by_id('subj_id')
+    select = Select(driver.find_element(by = 'id', value = 'subj_id'))
+    subj = driver.find_element(by = 'id', value = 'subj_id')
     subj.send_keys('c')
     for i in range(20) :
         subj.send_keys(Keys.DOWN)
         if select.first_selected_option.text == 'Computer Science' :
             break
-
-
 else : 
     # select instructor (direct selection does not work)
-    select = Select(driver.find_element_by_id('instr_id'))
+    select = Select(driver.find_element(by = 'id', value = 'instr_id'))
     name = driver.find_element_by_xpath("//select[@id='instr_id']/option[@value='8A92261F1A2BAAECB202A0A1EF704C67']")
     
     name.send_keys('D')
@@ -53,5 +50,5 @@ else :
 
 
 # submit
-btn = driver.find_element_by_name('SUB_BTN')
+btn = driver.find_element(by = 'name', value = 'SUB_BTN')
 btn.click()
